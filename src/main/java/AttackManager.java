4511e10b261ccc2a4d20c0a50ad8ca586f6b46c5
Joelle JEAN BAPTISTE <misssileina@gmail.com>
2019-07-10 12:34:12 +0200

    Interface Attack

---
+++ src/main/java/AttackManager.java
@@ -0,0 +1,4 @@
+public interface AttackManager {
+    public void attackSpecial( Personnage att,  Personnage cible);
+
+}
--- src/main/java/Mage.java
+++ src/main/java/Mage.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
 
-public class Mage {
+
+public class Mage implements AttackManager{
+    public Personnage joelle = new Personnage();
+    @Override
+    public void attackSpecial(Personnage att, Personnage cible) {
+
+    }
+
+    joelle.setNom("Mage");
+    joelle.setPv(311)
+
 }
