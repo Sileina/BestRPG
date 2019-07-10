f64ab03aa5b44e1722e36b0d7488d5078cb2027b
Joelle JEAN BAPTISTE <misssileina@gmail.com>
2019-07-10 12:20:18 +0200

    Personnage

--- src/main/java/Personnage.java
+++ src/main/java/Personnage.java
@@ -5,6 +5,56 @@ public class Personnage {
     private Integer attaque;
     private Integer defense;
     private Integer magie;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Integer getPv() {
+        return pv;
+    }
+
+    public void setPv(Integer pv) {
+        this.pv = pv;
+    }
+
+    public Integer getAttaque() {
+        return attaque;
+    }
+
+    public void setAttaque(Integer attaque) {
+        this.attaque = attaque;
+    }
+
+    public Integer getDefense() {
+        return defense;
+    }
+
+    public void setDefense(Integer defense) {
+        this.defense = defense;
+    }
+
+    public Integer getMagie() {
+        return magie;
+    }
+
+    public void setMagie(Integer magie) {
+        this.magie = magie;
+    }
+
+    public Integer getSoin() {
+        return soin;
+    }
+
+    public void setSoin(Integer soin) {
+        this.soin = soin;
+    }
+
     private Integer soin;
+
 
 }
