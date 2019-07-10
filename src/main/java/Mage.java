d5539fc9cac07e06b99e0ad41add6aacc2b28f23
Joelle JEAN BAPTISTE <misssileina@gmail.com>
2019-07-10 12:52:19 +0200

    Mage

--- src/main/java/Mage.java
+++ src/main/java/Mage.java
@@ -2,12 +2,22 @@
 
 public class Mage implements AttackManager{
     public Personnage joelle = new Personnage();
-    @Override
-    public void attackSpecial(Personnage att, Personnage cible) {
 
+    @Override
+    public void attack(Personnage att, Personnage cible) {
+        cible.setPv(cible.getPv - (att.getAttaque()-cible.getDefense()));
     }
 
+    @Override
+    public void attackSpecial(Personnage att, Personnage cible) {
+        cible.setPv(cible.getPv - (att.getMagie()*1.5-cible.getDefense()));
+    }
+
+
     joelle.setNom("Mage");
-    joelle.setPv(311)
+    joelle.setPv(311);
+    joelle.setAttaque(400);
+    joelle.setDefense(300);
+    joelle.getMagie(400);
 
 }
