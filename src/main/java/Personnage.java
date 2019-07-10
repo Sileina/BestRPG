350e75d8445606098d832c62ba4ccbd9c4d43edb
Joelle JEAN BAPTISTE <misssileina@gmail.com>
2019-07-10 11:57:34 +0200

    Def classe Personnage

--- src/main/java/Personnage.java
+++ src/main/java/Personnage.java
@@ -1,2 +1,10 @@
 public class Personnage {
+
+    private String nom;
+    private Integer pv;
+    private Integer attaque;
+    private Integer defense;
+    private Integer magie;
+    private Integer soin;
+
 }
